419108cd21fd1b1b15b149dc4609b7a1e45b51d1
Tim Schwartz <tim@metaverse.systems>
2018-01-24 14:15:17 -0600

    Fixed memory map decoding

--- main.c
+++ main.c
@@ -52,13 +52,19 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     Print(L"Memory map size: %d.\n", mapSize);
     Print(L"Memory descriptor size: %d.\n\n", descriptorSize);
 
+    uint8_t *startOfMemoryMap = (uint8_t *)memoryMap;
+    uint8_t *endOfMemoryMap = startOfMemoryMap + mapSize;
+
+    uint8_t *offset = startOfMemoryMap;
+
     uint32_t counter = 0; 
+    uint64_t totalPages = 0;
 
     EFI_MEMORY_DESCRIPTOR *desc = NULL;
 
-    while(counter < 3)  //(mapSize / descriptorSize))
+    while(offset < endOfMemoryMap)
     {
-        desc = memoryMap + counter;
+        desc = (EFI_MEMORY_DESCRIPTOR *)offset;
 
         Print(L"Map %d:\n", counter);
         Print(L"  Type: %X, %s\n", desc->Type, memory_types[desc->Type]); 
@@ -66,8 +72,15 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
         Print(L"  VirtualStart: %X\n", desc->VirtualStart);
         Print(L"  NumberOfPages: %X   (4k)\n", desc->NumberOfPages);
         Print(L"  Attribute: %X\n", desc->Attribute);
+
+        totalPages += desc->NumberOfPages;
+
+        offset += descriptorSize;
         counter++;
     }
+
+    uint64_t memorySize = totalPages * 4096;
+    Print(L"Memory detected: %d MB\n", memorySize / 1024 / 1024);
 
     for(;;);
 
